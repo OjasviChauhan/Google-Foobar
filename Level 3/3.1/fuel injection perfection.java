4033825dbedb87a16cfb5c543f63ff3d7e7c7370
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-09 00:30:05 +0530

    Update fuel injection perfection.java

--- Level 3/3.1/fuel injection perfection.java
+++ Level 3/3.1/fuel injection perfection.java
@@ -2,16 +2,26 @@ package com.google.challenges;
 public class Solution {
     public static String solution(String x) {
         int n=Integer.parseInt(x);
-        int z=countways(n);
+        int dp[] = new int[1000];
+        int z=countways(n,dp);
         String s=Integer.toString(z);
         return s;
     }
-    static int countways(int n) { 
-        if (n == 1) 
-            return 0; 
-        else if (n % 2 == 0) 
-            return 1 + countways(n / 2); 
-        else
-            return 1 + Math.min(countways(n - 1),countways(n + 1)); 
-    } 
+    
+    static int countways(int n,int dp[]){
+       if(n==1) return 0; // Base Case
+       
+       if(dp[n]!=0) return dp[n]; // Dp Lookup
+
+       if(n%2==0) return countways(n/2,dp) + 1; //op1
+       int ans = Math.min(countways(n-1,dp),countways(n+1,dp)) + 1; // min(op2,op3)
+       return dp[n] = ans; //stored in dp array before returning
+    }
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        String s = solution(x);
+        System.out.println(s);
+    }
 }
